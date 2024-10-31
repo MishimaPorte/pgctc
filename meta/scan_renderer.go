bee961481bffc18f9a9e3cbfe1a1449365ec7a1e
abk <porte@apsolutions.ru>
2024-10-31 17:55:57 +0300

    fixed renderer

--- meta/scan_renderer.go
+++ meta/scan_renderer.go
@@ -38,7 +38,7 @@ func (g *generator) renderDriverScan(t reflect.Type) {
 	sourceBytes = unsafeBytes(source)
 
 	var items = make([]%s, 0)
-	for cur := 0; cur < sourceLen; {
+	for cur := 1; cur < sourceLen; {
 		var n int
 		var a string
 		if a, n, err = readString(sourceBytes[cur:], '}'); err != nil {
