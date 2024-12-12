8660e423db96f4acd5718b2b3f96e03429564be9
abk <porte@apsolutions.ru>
2024-12-12 16:39:26 +0300

    fix

--- meta/value_renderer.go
+++ meta/value_renderer.go
@@ -73,6 +73,7 @@ func (g *generator) renderNullableStructFieldMarshaller(field *reflect.StructFie
 
 func (g *generator) renderSimpleToString(t reflect.Type, name string) {
 	if t.Implements(reflect.TypeOf((*driver.Valuer)(nil)).Elem()) || reflect.PointerTo(t).Implements(reflect.TypeOf((*driver.Valuer)(nil)).Elem()) {
+		g.addImport("time")
 		g.fprintf(`
 		var thing string
 		if a, b := %s.Value(); b != nil {
